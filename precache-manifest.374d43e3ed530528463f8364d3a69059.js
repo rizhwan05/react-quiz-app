a843a84796df4575c62a87979376972a6eba74ee
= <rizhwan2005@gmail.com>
2024-07-20 12:15:03 +0530

    Updates

---
+++ precache-manifest.374d43e3ed530528463f8364d3a69059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8fbfab41777c203d3f788491c8dcb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb76f7b61fa44f4743ce",
+    "url": "/static/css/main.47227cb2.chunk.css"
+  },
+  {
+    "revision": "95e1f0f803acc61e2952",
+    "url": "/static/js/2.9f140c04.chunk.js"
+  },
+  {
+    "revision": "bb76f7b61fa44f4743ce",
+    "url": "/static/js/main.bee7b6cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5938b2e252817f82e170d9f62183f3a9",
+    "url": "/static/media/img1.5938b2e2.jpg"
+  }
+]);
